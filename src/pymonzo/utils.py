824f7ed113544e222f1522b4b6e37220ddafd977
Paweł Adamczak <pawel.ad@gmail.com>
2022-08-18 23:59:00 +0200

    Delete 'utils.CommonMixin'

--- src/pymonzo/utils.py
+++ src/pymonzo/utils.py
@@ -6,31 +6,6 @@ from wsgiref.simple_server import make_server
 from wsgiref.util import request_uri
 
 
-class CommonMixin(object):
-    """
-    Common class mixin that implements sensible defaults
-    """
-
-    def __eq__(self, other):
-        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __str__(self):
-        data = ", ".join(
-            [
-                "{}={}".format(k, v)
-                for k, v in self.__dict__.items()
-                if not k.startswith("_")
-            ]
-        )
-        return "{}({})".format(self.__class__.__name__, data)
-
-    def __repr__(self):
-        return "{}({})".format(self.__class__, self.__dict__)
-
-
 class WSGIApp:
     """
     Bare-bones WSGI app made for retrieving the OAuth callback.
