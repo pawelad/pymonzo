49f5751add1593c4afa4e690e25a9303cbb41a10
Paweł Adamczak <pawel.ad@gmail.com>
2022-09-06 23:34:59 +0200

    Add undocumented API fields to 'MonzoPot'

--- src/pymonzo/pots/schemas.py
+++ src/pymonzo/pots/schemas.py
@@ -2,6 +2,7 @@
 Monzo API pots related schemas.
 """
 from datetime import datetime
+from typing import Optional
 
 from pydantic import BaseModel
 
@@ -22,3 +23,17 @@ class MonzoPot(BaseModel):
     created: datetime
     updated: datetime
     deleted: bool
+
+    # Undocumented in API docs
+    goal_amount: int
+    type: str
+    product_id: str
+    current_account_id: str
+    cover_image_url: str
+    isa_wrapper: str
+    round_up: bool
+    round_up_multiplier: Optional[int]
+    is_tax_pot: bool
+    locked: bool
+    available_for_bills: bool
+    has_virtual_cards: bool
