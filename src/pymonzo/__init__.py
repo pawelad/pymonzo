0b6b0f30ea074e1513b19bac968d6f7b8143e3e6
Paweł Adamczak <pawel.ad@gmail.com>
2023-04-03 16:47:37 +0200

    Update package 'dunder' attributes

--- src/pymonzo/__init__.py
+++ src/pymonzo/__init__.py
@@ -2,7 +2,9 @@
 from pymonzo.client import MonzoAPI  # noqa
 
 __title__ = "pymonzo"
-__version__ = "0.22.0.dev0"
+__description__ = "Modern Python API client for Monzo public API"
+__version__ = "1.0.0.dev0"
+__url__ = "https://github.com/pawelad/pymonzo"
 __author__ = "Paweł Adamczak"
 __email__ = "pawel.ad@gmail.com"
-__url__ = "https://github.com/pawelad/pymonzo"
+__license__ = "MPL-2.0"
