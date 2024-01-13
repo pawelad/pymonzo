213b93319dbeda453bbd6dd6192e266fca215731
Paweł Adamczak <pawel.ad@gmail.com>
2024-01-13 20:59:54 +0100

    Add and use `vcrpy-encrypt`

--- tests/pymonzo/conftest.py
+++ tests/pymonzo/conftest.py
@@ -1,12 +1,30 @@
 """pymonzo pytest configuration and utils."""
+import os
+
 import pytest
+from _pytest.config import Config
+from vcr import VCR
+from vcrpy_encrypt import BaseEncryptedPersister
 
 from pymonzo import MonzoAPI
+
+VCRPY_ENCRYPTION_KEY = os.getenv("VCRPY_ENCRYPTION_KEY", "").encode("UTF-8")
+
+
+class PyMonzoEncryptedPersister(BaseEncryptedPersister):
+    """Custom VCR persister that encrypts cassettes."""
+
+    encryption_key: bytes = VCRPY_ENCRYPTION_KEY
+
+
+def pytest_recording_configure(config: Config, vcr: VCR) -> None:
+    """Register custom VCR persister that encrypts cassettes."""
+    vcr.register_persister(PyMonzoEncryptedPersister)
 
 
 @pytest.fixture(scope="module")
 def monzo_api() -> MonzoAPI:
-    """Return a 'MonzoAPI' instance."""
+    """Return a `MonzoAPI` instance."""
     return MonzoAPI(
         client_id="TEST_CLIENT_ID",
         client_secret="TEST_CLIENT_SECRET",  # noqa
