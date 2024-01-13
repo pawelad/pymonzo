01340c4d8613f19a3e7592bf38e2005d6b79fac4
Paweł Adamczak <pawel.ad@gmail.com>
2024-01-13 22:33:33 +0100

    Add `TestAttachmentsResource.test_register_respx`

--- tests/pymonzo/test_attachments.py
+++ tests/pymonzo/test_attachments.py
@@ -1,11 +1,21 @@
 """Test `pymonzo.attachments` module."""
+import json
+
 import httpx
 import pytest
 import respx
 from polyfactory.factories.pydantic_factory import ModelFactory
 
 from pymonzo import MonzoAPI
-from pymonzo.attachments import AttachmentsResource, MonzoAttachmentResponse
+from pymonzo.attachments import (
+    AttachmentsResource,
+    MonzoAttachment,
+    MonzoAttachmentResponse,
+)
+
+
+class MonzoAttachmentFactory(ModelFactory[MonzoAttachment]):
+    """Factory for `MonzoAttachment` schema."""
 
 
 class MonzoAttachmentResponseFactory(ModelFactory[MonzoAttachmentResponse]):
@@ -49,4 +59,41 @@ class TestAttachmentsResource:
         )
 
         assert attachment_upload_response == attachment_response
+        assert mocked_route.called
+
+    @pytest.mark.respx(base_url=MonzoAPI.api_url)
+    def test_register_respx(
+        self,
+        respx_mock: respx.MockRouter,
+        attachments_resource: AttachmentsResource,
+    ) -> None:
+        """Correct API response is sent, API response is parsed into expected schema."""
+        transaction_id = "TEST_TRANSACTION_ID"
+        file_url = "TEST_FILE_URL"
+        file_type = "TEST_FILE_TYPE"
+        attachment = MonzoAttachmentFactory.build()
+
+        mocked_route = respx_mock.post(
+            "/attachment/register",
+            params={
+                "external_id": transaction_id,
+                "file_url": file_url,
+                "file_type": file_type,
+            },
+        ).mock(
+            return_value=httpx.Response(
+                200,
+                # TODO: Change when updating to Pydantic 2
+                #   `attachment.model_dump(mode='json')`
+                json={"attachment": json.loads(attachment.json())},
+            )
+        )
+
+        attachment_register_response = attachments_resource.register(
+            transaction_id,
+            file_url=file_url,
+            file_type=file_type,
+        )
+
+        assert attachment_register_response == attachment
         assert mocked_route.called
