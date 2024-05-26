ba167fa6b27f83a3601dd9442f70cc85e453b47f
m-roberts <2947595+m-roberts@users.noreply.github.com>
2024-05-26 17:22:03 +0100

    Fix pot deposit handling

--- src/pymonzo/resources.py
+++ src/pymonzo/resources.py
@@ -28,6 +28,7 @@ class BaseResource:
         method: str,
         endpoint: str,
         params: Optional[dict] = None,
+        data: Optional[dict] = None,
     ) -> httpx.Response:
         """Handle HTTP requests and catch API errors.
 
@@ -43,7 +44,7 @@ class BaseResource:
             MonzoAccessDenied: When access to Monzo API was denied.
             MonzoAPIError: When Monzo API returned an error.
         """
-        response = getattr(self.client.session, method)(endpoint, params=params)
+        response = getattr(self.client.session, method)(endpoint, params=params, data=data)
 
         if response.status_code == codes.FORBIDDEN:
             raise MonzoAccessDenied(
